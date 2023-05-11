d544471c4bcbbffc2fd467ae247b7c90f4644631
Thiago Valle <zthiagovalle@gmail.com>
2023-05-10 23:19:42 -0300

    terminado todas rotas do crud com testes

--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -7,20 +7,21 @@ router.get('/posts', async function (req, res) {
     res.json(posts);
 });
 
-router.get('/posts/:id', async function (req, res) {
-
-});
-
 router.post('/posts', async function (req, res) {
-
+    const post = req.body;
+    const newPost = await postsService.savePost(post);
+    res.json(newPost);
 });
 
 router.put('/posts/:id', async function (req, res) {
-
+    const post = req.body;
+    await postsService.updatePost(req.params.id, post);
+    res.end();
 });
 
 router.delete('/posts/:id', async function (req, res) {
-
+    await postsService.deletePost(req.params.id);
+    res.end();
 });
 
 module.exports = router;
